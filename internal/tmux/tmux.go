37b4fe6cc0c9aae4342d0aaeaed158de35e8963e
Jared Griggs <griggsjared@gmail.com>
2026-03-26 20:13:07 -0700

    docs: add some basic tmux type and method docs

--- internal/tmux/tmux.go
+++ internal/tmux/tmux.go
@@ -9,30 +9,37 @@ import (
 	"github.com/griggsjared/tm/internal/session"
 )
 
+// Runner defines an interface for running and executing tmux commands.
 type Runner interface {
 	Run(path string, args []string) ([]byte, error)
 	Exec(path string, args []string) error
 }
 
+// TmuxRunner is a concrete implementation of Runner that uses the os/exec package to run tmux commands.
 type TmuxRunner struct{}
 
+// NewRunner creates a new instance of TmuxRunner.
 func NewRunner() *TmuxRunner {
 	return &TmuxRunner{}
 }
 
+// Run executes a tmux command and returns its output or an error if the command fails.
 func (t *TmuxRunner) Run(path string, args []string) ([]byte, error) {
 	return exec.Command(path, args...).Output()
 }
 
+// Exec executes a tmux command and replaces the current process with the new command.
 func (t *TmuxRunner) Exec(path string, args []string) error {
 	return syscall.Exec(path, args, os.Environ())
 }
 
+// Repository provides methods to interact with tmux sessions using a Runner.
 type Repository struct {
 	runner Runner
 	path   string
 }
 
+// NewRepository creates a new Repository with the given Runner and tmux path.
 func NewRepository(r Runner, path string) *Repository {
 	return &Repository{
 		runner: r,
@@ -40,6 +47,7 @@ func NewRepository(r Runner, path string) *Repository {
 	}
 }
 
+// HasSession checks if a tmux session with the given name exists.
 func (t *Repository) HasSession(name string) bool {
 	if _, err := t.runner.Run(t.path, []string{"has-session", "-t", name}); err != nil {
 		return false
@@ -47,14 +55,17 @@ func (t *Repository) HasSession(name string) bool {
 	return true
 }
 
+// NewSession creates a new tmux session with the given Session details.
 func (t *Repository) NewSession(s *session.Session) error {
 	return t.runner.Exec(t.path, []string{"tmux", "new-session", "-s", s.Name, "-c", s.Dir})
 }
 
+// AttachSession attaches to an existing tmux session with the given Session details.
 func (t *Repository) AttachSession(s *session.Session) error {
 	return t.runner.Exec(t.path, []string{"tmux", "attach-session", "-t", s.Name})
 }
 
+// AllSessions retrieves all tmux sessions and returns them as a slice of Session pointers.
 func (t *Repository) AllSessions() []*session.Session {
 	var sessions []*session.Session
 	output, err := t.runner.Run(t.path, []string{"list-sessions", "-F", "#{session_name}:#{session_path}"})
